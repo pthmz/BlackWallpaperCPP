014ec5fe274e7f1534de9e992a91016591d64920
User <User@DESKTOP-82EM2E1>
2024-12-08 00:28:28 +0800

    添加项目文件。

---
+++ BlackWallpaperCPP/BlackWallpaperCPP.cpp
@@ -0,0 +1,18 @@
+﻿#include <windows.h>
+#include <iostream>
+
+int main() {
+    const wchar_t* path = L"C:\\black_pixel.bmp";
+    int result;
+
+    result = SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, (void*)path, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+
+    if (result) {
+        std::wcout << L"Wallpaper set successfully.\n";
+    }
+    else {
+        std::wcout << L"Failed to set wallpaper.\n";
+    }
+
+    return 0;
+}
